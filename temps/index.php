8102af06dec97e99dbc65f67c21fdae60488cd89
andersonandernet <anderson@andernet.com.br>
2021-07-05 20:44:25 -0300

    Update index.php

--- temps/index.php
+++ temps/index.php
@@ -8,7 +8,7 @@
 
 <table>
 <tr>
-    <td>Anderson - Enter the temperature to convert:</td>
+    <td>Anderson 2 - Enter the temperature to convert:</td>
     <td><input type="text" name="temp" id="temp" size="10"></td>
 </tr>
 
